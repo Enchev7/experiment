fb08fea3ede7d855f17f39db415ddec3eb8bb69a
Encho <encho.enchev0@gmail.com>
2023-04-14 10:01:42 +0300

    Intial spring project commit

---
+++ src/main/java/com/example/demo/model/DTOs/AbstractDTO.java
@@ -0,0 +1,13 @@
+package com.example.demo.model.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class AbstractDTO {
+}
---
+++ src/main/java/com/example/demo/model/DTOs/UserRegisterDataDTO.java
@@ -0,0 +1,10 @@
+package com.example.demo.model.DTOs;
+
+
+public class UserRegisterDataDTO extends AbstractDTO {
+
+    private String fullName;
+    private String email;
+    private String password;
+    private int age;
+}
